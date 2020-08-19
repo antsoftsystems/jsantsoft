b833c1ac6f40f7d93940fae7968f53c3cb915855
AntSoft Systems On Demand <suporte@antsoft.com.br>
2020-08-18 21:41:25 -0300

    Update to include hhtps
    Include https protocol, on URI's

--- antsoftbanner.js
+++ antsoftbanner.js
@@ -13,11 +13,12 @@
 // » 0.6 - Substituição da tag <small> pela tag <p> e inclusão da class small no texto Developer....
 // » 0.7 - Alteração da tag <center> pela style text-align:center no texto Developer....
 // » 0.8 - Migração para GitHub, tornando código aberto
+// » 0.8 - Incluido o protocolo https, devido a novas polpiticas e segurança do site da AntSoft
 (function ($) {
     "use strict"; // Start of use strict
     $(document).ready(function () {
-        document.getElementById("antsoft").innerHTML = "<p class='small' style='text-align:center'><a href='http://www.antsoft.com.br' target='_blank'><img src='http://www.antsoft.com.br/images/ant_soft_logomarca_small.png'/><br />Developer by: AntSoft Systems On Demand</small></a></p>";
-        document.getElementsByClassName("antsoft").innerHTML = "<p class='small' style='text-align:center'><a href='http://www.antsoft.com.br' target='_blank'><img src='http://www.antsoft.com.br/images/ant_soft_logomarca_small.png'/><br />Developer by: AntSoft Systems On Demand</a></p>";
+        document.getElementById("antsoft").innerHTML = "<p class='small' style='text-align:center'><a href='https://www.antsoft.com.br' target='_blank'><img src='https://www.antsoft.com.br/images/ant_soft_logomarca_small.png'/><br />Developer by: AntSoft Systems On Demand</small></a></p>";
+        document.getElementsByClassName("antsoft").innerHTML = "<p class='small' style='text-align:center'><a href='https://www.antsoft.com.br' target='_blank'><img src='https://www.antsoft.com.br/images/ant_soft_logomarca_small.png'/><br />Developer by: AntSoft Systems On Demand</a></p>";
       
     });
 })(jQuery); // End of use strict
